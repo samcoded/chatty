3617a795fb2c64d4b06a2db905770b3b83c14aa9
Samuel Anozie <samuelanozie10@yahoo.com>
2023-12-15 22:13:32 +0100

    Message model

---
+++ server/models/Message.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const messageSchema = new Schema({
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiver: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+export default Message;
